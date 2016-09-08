97401358d79c681e40afe7864b0415b1629b5929
Martin <cauu@163.com>
2016-09-08 17:05:52 +0800

    修改rake;post node-sass-install

--- Rakefile.rb
+++ Rakefile.rb
@@ -47,7 +47,7 @@ task :post do
     post.puts "---"
     post.puts "layout: post"
     post.puts "title: #{title.gsub(/-/,' ')}"
-    post.puts "category: "
+    post.puts "categories: "
     post.puts "date: #{date}"
     post.puts "---"
   end
